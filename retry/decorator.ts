8bafbcfd5a509afdb587e8929e97038915b275a8
Utilisateur <franck.ledoux.pro@gmail.com>
2020-12-19 19:11:10 +0100

    retry async decorator refacto

--- retry/decorator.ts
+++ retry/decorator.ts
@@ -1,8 +1,6 @@
 import { retryAsync } from "./retry.ts";
 import { RetryOptions } from "./options.ts";
 
-type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T;
-
 export function retryAsyncDecorator<
   // deno-lint-ignore no-explicit-any
   T extends (...args: any[]) => Promise<any>,
@@ -10,7 +8,6 @@ export function retryAsyncDecorator<
   fn: T,
   retryOptions?: RetryOptions,
 ) {
-  type U = ReturnType<T>;
   return (...args: Parameters<T>): ReturnType<T> => {
     const wrappedFn = () => fn(...args);
     return retryAsync(wrappedFn, retryOptions) as ReturnType<T>;
