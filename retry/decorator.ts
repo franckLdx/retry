a6c70c04f7055af5c0d2ae02ca9dddd4c968aea3
Utilisateur <franck.ledoux.pro@gmail.com>
2020-12-19 19:17:07 +0100

    retry decorator added

--- retry/decorator.ts
+++ retry/decorator.ts
@@ -1,4 +1,4 @@
-import { retryAsync } from "./retry.ts";
+import { retry, retryAsync } from "./retry.ts";
 import { RetryOptions } from "./options.ts";
 
 export function retryAsyncDecorator<
@@ -11,5 +11,18 @@ export function retryAsyncDecorator<
   return (...args: Parameters<T>): ReturnType<T> => {
     const wrappedFn = () => fn(...args);
     return retryAsync(wrappedFn, retryOptions) as ReturnType<T>;
+  };
+}
+
+export function retryDecorator<
+  // deno-lint-ignore no-explicit-any
+  T extends (...args: any[]) => any,
+>(
+  fn: T,
+  retryOptions?: RetryOptions,
+) {
+  return (...args: Parameters<T>): Promise<ReturnType<T>> => {
+    const wrappedFn = () => fn(...args);
+    return retry(wrappedFn, retryOptions) as Promise<ReturnType<T>>;
   };
 }
--- retry/decorators_test.ts
+++ retry/decorators_test.ts
@@ -1,5 +1,5 @@
 import { deferred } from "https://deno.land/std@0.81.0/async/deferred.ts";
-import { retryAsyncDecorator } from "./decorator.ts";
+import { retryAsyncDecorator, retryDecorator } from "./decorator.ts";
 import { assertEquals, assertThrowsAsync } from "../dev_deps.ts";
 
 Deno.test({
@@ -27,6 +27,36 @@ Deno.test({
       return promise;
     };
     const decorated = retryAsyncDecorator(fn, { maxTry: 2, delay: 50 });
+    await assertThrowsAsync(
+      async () => await decorated(),
+      Error,
+      errorMsg,
+    );
+    assertEquals(callCount, 2);
+  },
+});
+
+Deno.test({
+  name: "decorator should return the valid result",
+  fn: async () => {
+    const fn = (p1: string, p2: number) => {
+      return Number(p1) * p2;
+    };
+    const decorated = retryDecorator(fn);
+    assertEquals(await decorated("10", 2), 20);
+  },
+});
+
+Deno.test({
+  name: "decorator should throw an exception",
+  fn: async () => {
+    let callCount = 0;
+    const errorMsg = "BOOM";
+    const fn = () => {
+      callCount++;
+      throw new Error(errorMsg);
+    };
+    const decorated = retryDecorator(fn, { maxTry: 2, delay: 50 });
     await assertThrowsAsync(
       async () => await decorated(),
       Error,
